a7719f50000673af4a08d4b04b8ae3d542f99425
Pat Welch <pat@mousebrains.com>
2025-01-23 11:31:42 +1000

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# RecoverBy
-When will a buoyancy glider need to be recovered by?
+# Estimate when a Slocum glider will need to be recovered by
+
+The main method uses the estimated percentage of battery remaining to predict
+when the percentage left will be a given value.
+
+The input is a time series NetCDF with at least two columns:
+ - time a CF compliant time of the observation
+ - m\_lithium\_battery\_relative\_charge as defined by Slocum masterdata file
+
+A compatible input NetCDF file is generated from my (Slocum_DataHarvester)[https://github.com/mousebrains/ARCTERX_Slocum_DataHarvester] in the sensors files.
+
+A linear regression is done to:
+
+m\_lithium\_battery\_relative\_charge = a + b (time - min(time))/86400
+
+so the slope is in days.
---
+++ SlocumBatteryPercentageDuration.py
@@ -0,0 +1,109 @@
+#! /usr/bin/env python3
+#
+# Calculate when a Slocum glider will need to be recovered by 
+# from the battery percentage used.
+#
+# There are a lot of assumptions here, such as constant in time usage.
+# If your operation is changing modes, please don't use this, or
+# set the start/end times appropriately.
+#
+# Jan-2025, Pat Welch
+
+from argparse import ArgumentParser
+import numpy as np
+import xarray as xr
+import logging
+from scipy.stats import linregress
+from matplotlib import pyplot as plt
+import os
+
+parser = ArgumentParser(
+        description="Slocum recover by estimates",
+        epilog="""
+Calculate when a Slocum glider will need to be recovered by 
+from the battery percentage used.
+There are a lot of assumptions here, such as constant in time usage.
+If your operation is changing modes, please don't use this, or
+set the start/end times appropriately.
+""",
+        )
+parser.add_argument("--verbose", action="store_true", help="Enable debugging messages")
+parser.add_argument("filename", type=str, nargs="+", 
+                    help="Input filename(s) containing the" 
+                    + " m_present_time and m_lithium_battery_relative_charge variables")
+parser.add_argument("--start", type=str, help="Only use data after this UTC time.")
+parser.add_argument("--stop", type=str, help="Only use data before this UTC time.")
+parser.add_argument("--threshold", type=float, default=15,
+                    help="Percentage at which recovery should happen")
+parser.add_argument("--plot", action="store_true", help="Generate a plot")
+args = parser.parse_args()
+
+logging.basicConfig(
+        level = logging.DEBUG if args.verbose else logging.INFO,
+        format = "%(asctime)s %(levelname)s: %(message)s",
+        )
+
+if args.plot:
+    [fig, axs] = plt.subplots(len(args.filename), 1, sharex=True)
+    fig.subplots_adjust(hspace=0)
+
+for index in range(len(args.filename)):
+    fn = args.filename[index]
+    with xr.open_dataset(fn) as ds:
+        varNames = ("time", "m_lithium_battery_relative_charge")
+        for name in varNames:
+            if name not in ds:
+                logging.error("time variable not present in %s", fn)
+                continue
+        ds = ds.drop_vars(filter(lambda x: x not in varNames, ds))
+        ds = ds.drop_duplicates("time", keep="first")
+        ds = ds.sel(time=ds.time[np.logical_not(ds.m_lithium_battery_relative_charge.isnull())])
+        ds = ds.sortby("time") # here before slicing
+        if args.start is not None or args.stop is not None:
+            if args.start is not None:
+                stime = np.datetime64(args.start)
+            else:
+                stime = ds.time[0]
+            if args.stop is not None:
+                etime = np.datetime64(args.stop)
+            else:
+                etime = ds.time[-1]
+            ds = ds.sel(time=slice(stime, etime))
+
+        if ds.time.size == 0:
+            logging.error("No data to fit in %s", fn)
+            continue
+
+        ds["dDays"] = (ds.time.data - ds.time.data[0]).astype(float) / 86400e9
+
+        mdl = linregress(ds.dDays, ds.m_lithium_battery_relative_charge)
+
+        dRecovery = (args.threshold - mdl.intercept) / mdl.slope
+        tRecoverBy = ds.time[0].data + dRecovery.astype("timedelta64[D]")
+        tRecoverBy = tRecoverBy.astype("datetime64[s]")
+
+        print("\n", fn)
+        print(f"Intercept: {mdl.intercept:.4f}+-{mdl.intercept_stderr:.4f}")
+        print(f"Slope:     {mdl.slope:.4f}+-{mdl.stderr:.4f}")
+        print(f"Rvalue:    {mdl.rvalue:.4f}")
+        print(f"Pvalue:    {mdl.pvalue:.4f}")
+        print(f"Recovery By: {tRecoverBy}")
+
+        if args.plot:
+            slope = -mdl.slope
+            iTit = os.path.basename(fn) + "\n% battery remaining"
+            fitTit = f"{mdl.intercept:.1f}-{slope:.2f} * days"
+            fitTit+= f"\nRecovery by {tRecoverBy}"
+            ax = axs[index]
+            ax.plot(ds.time, ds.m_lithium_battery_relative_charge, "o", label=iTit)
+            ax.plot(ds.time, mdl.intercept + mdl.slope * ds.dDays, "r", label=fitTit)
+            ax.set_ylabel("% of battery remaining")
+            ax.legend()
+            ax.grid()
+
+if args.plot:
+    ax = axs[-1]
+    ax.set_xlabel("Time (UTC)")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
